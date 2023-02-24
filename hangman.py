837a0f234824bf20ee422dd30ca180702c2233d6
Dixit Dhiman <dixitdhiman144@gmail.com>
2023-02-24 13:41:53 +0530

    Adding the functionalty of lives

--- hangman.py
+++ hangman.py
@@ -14,9 +14,11 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
 
-    while len(word_letters) > 0:
+    lives = 8 
+
+    while len(word_letters) > 0 and lives > 0:
         #letter used
-        print("You have used these letters: ", ' '.join(used_letters))
+        print("You have ",lives," lives left and You have used these letters: ", ' '.join(used_letters))
 
         #ehat currend word is (ie W - R D)
         word_list = [letter if letter in used_letters else '-' for letter in word]
@@ -29,10 +31,17 @@ def hangman():
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+            else:
+                lives -= 1
         elif user_letter in used_letters:
             print('You have already used That character! Please try again.')
         else:
             print('Invalid character. Please try again')
+    
+    if lives == 0:
+        print('You died, sorry. The word was: ', word)
+    else:
+        print('You guessed the word ', word, '!!')
 
 
 # user_input = input('Entre Something : ').upper()
